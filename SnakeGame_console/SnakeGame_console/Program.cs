ffd3dc3cf413e8d576cd0de3c96b7ea208307684
overlord243 <loloverlord2@gmail.com>
2015-12-14 17:02:01 +0400

    какой то бред со списками

--- SnakeGame_console/SnakeGame_console/Program.cs
+++ SnakeGame_console/SnakeGame_console/Program.cs
@@ -15,7 +15,20 @@ namespace SnakeGame_console
        
                 Point p2 = new Point(4,5,'#');
                 p2.Draw();
-        
+
+                Point p3 = new Point(7, 9, '#');
+                p3.Draw();
+
+
+                List<int> numList = new List<int>();
+                numList.Add(0);
+                numList.Add(1);
+                numList.Add(2);
+
+                int x = numList[0];
+            int y= numList[1];
+            int z=numList[2];
+
 
 
             //  int x1 = 1;
@@ -27,6 +40,17 @@ namespace SnakeGame_console
             //   int y2 = 5;
             //  char sym2 = '#';
             //  Draw(x2, y2, sym2);
+
+ numList.RemoveAt(0);
+
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+           
 
             Console.ReadLine();
         }
